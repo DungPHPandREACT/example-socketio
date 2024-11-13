7e97a58aa0074e6f55a8e86c7a95cb2999d831cd
DungPHPAndReact <dung2000cn@gmail.com>
2024-11-13 21:16:25 +0700

    example rooms

--- client/public/main.js
+++ client/public/main.js
@@ -2,20 +2,56 @@ const socket = io('http://localhost:3001', {
 	transport: ['websocket'],
 });
 
-socket.on('message', (message) => {
+let currentRoom = '';
+
+socket.on('connect', () => {
+	console.log(`Kết nối thành công với ID: ${socket.id}`);
+});
+
+const writMessage = (message) => {
 	const li = document.createElement('li');
 	li.textContent = message;
 	document.getElementById('messages').appendChild(li);
+};
+
+socket.on('message', (message) => {
+	console.log(`Tin nhắn từ server: ${message}`);
+	writMessage(message);
 });
 
-document.getElementById('send').onclick = () => {
-	const message = document.getElementById('message').value;
+const leaveRoom = () => {
+	if (currentRoom) {
+		socket.emit('leaveRoom', currentRoom);
+		const message = `Đang rời phòng ${currentRoom}`;
+		writMessage(message);
+		currentRoom = '';
+	}
+};
 
-	if (message) {
-		const li = document.createElement('li');
-		li.textContent = `Bạn: ${message}`;
-		document.getElementById('messages').appendChild(li);
-		socket.emit('message', message);
-		document.getElementById('message').value = '';
+document.getElementById('leave').onclick = leaveRoom;
+
+document.getElementById('join').onclick = () => {
+	const room = document.getElementById('room').value;
+	if (room) {
+		if (currentRoom) {
+			leaveRoom();
+		}
+
+		socket.emit('joinRoom', room);
+		currentRoom = room;
+		document.getElementById('messages').innerHTML = '';
+		const message = `Bạn đã tham gia phòng ${room}`;
+		writMessage(message);
+	}
+};
+
+document.getElementById('send').onclick = () => {
+	const value = document.getElementById('message').value;
+	if (value && currentRoom) {
+		socket.emit('message', currentRoom, value);
+		const message = `Bạn: ${value}`;
+		writMessage(message);
+	} else if (!currentRoom) {
+		alert('Bạn cần tham gia một phòng trước');
 	}
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io';
-import { socketHandler } from './socket';
+import { Server, Socket } from 'socket.io';
+
 
 const app = express();
 const server = http.createServer(app);
@@ -13,7 +13,28 @@ const io = new Server(server, {
     }
 })
 
-io.on('connection', socketHandler);
+io.on('connection',  (socket: Socket) =>{
+    socket.on('joinRoom', (room: string) => {
+        socket.join(room);
+        console.log(`${socket.id} đã tham gia phòng: ${room}`);
+        socket.to(room).emit('message', `Người dùng ${socket.id} đã tham gia phòng`)
+    })
+
+    socket.on('leaveRoom', (room: string) => {
+        socket.leave(room);
+        console.log(`${socket.id} đã rời phòng: ${room}`);
+        socket.to(room).emit('message', `Người dùng ${socket.id} đã rời phòng`)
+    })
+
+    socket.on('message', (room: string, message: string) => {
+        console.log(`Tin nhắn từ phòng ${room}: ${message}`);
+        io.to(room).emit('message', `${socket.id}: ${message}`)
+    })
+
+    socket.on('disconnect', () =>{
+        console.log(`Người dùng ngắt kết nối: ${socket.id}` )
+    })
+});
 
 const PORT = 3001;
 
